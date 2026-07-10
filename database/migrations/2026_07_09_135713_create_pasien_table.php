252d5add7b539f19c592b03c26d5bffaeaa8a117
Dini Sri Ayu Priyono <dinisriayupriyono@gmail.com>
2026-07-10 10:36:13 +0700

    update pasien dan booking

--- database/migrations/2026_07_09_135713_create_pasien_table.php
+++ database/migrations/2026_07_09_135713_create_pasien_table.php
@@ -6,29 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
     public function up(): void
     {
-        Schema::create('pasiens', function (Blueprint $table) {
+        Schema::create('pasien', function (Blueprint $table) {
+
             $table->id('id_pasien');
 
             $table->unsignedBigInteger('id_pengguna');
 
             $table->date('tanggal_lahir');
-            $table->string('jenis_kelamin');
-            $table->string('no_hp');
+            $table->enum('jenis_kelamin',['Laki-laki','Perempuan']);
+            $table->string('no_hp',20);
             $table->text('alamat');
 
             $table->timestamps();
 
             $table->foreign('id_pengguna')
-                  ->references('id_pengguna')
-                  ->on('penggunas')
-                  ->onDelete('cascade');
+                ->references('id_pengguna')
+                ->on('pengguna')
+                ->cascadeOnDelete();
+
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('pasiens');
+        Schema::dropIfExists('pasien');
     }
 };

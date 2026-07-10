5362d65085782651c31e00b908fa8520d0d9a150
Dini Sri Ayu Priyono <dinisriayupriyono@gmail.com>
2026-07-10 11:45:54 +0700

    feat: update pasien dan booking

---
+++ database/migrations/2026_07_09_135814_create_booking_consultations_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_consultations', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('patient_id')
+                  ->constrained('patients')
+                  ->cascadeOnDelete();
+
+            $table->foreignId('doctor_id')
+                  ->constrained('doctors')
+                  ->cascadeOnDelete();
+
+            $table->foreignId('schedule_id')
+                  ->constrained('doctor_schedules')
+                  ->cascadeOnDelete();
+
+            $table->date('booking_date');
+
+            $table->time('booking_time');
+
+            $table->enum('status', [
+                'pending',
+                'confirmed',
+                'completed',
+                'cancelled'
+            ])->default('pending');
+
+            $table->text('notes')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_consultations');
+    }
+};

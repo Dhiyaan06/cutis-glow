d1d59d8314dc36fb5f6b3375f588f075b2ee71ae
Dini Sri Ayu Priyono <dinisriayupriyono@gmail.com>
2026-07-13 06:21:45 +0700

    Add JadwalDokter functionality with controller, model, and migration
    - Created JadwalDokterController for managing doctor schedules with methods for index, create, store, edit, update, and destroy.
    - Implemented role-based access control for doctors and admins in the controller.
    - Added JadwalDokter model to interact with the jadwal_dokter table.
    - Created migration for jadwal_dokter table with necessary fields and constraints.

---
+++ database/migrations/2026_07_12_141004_create_jadwal_dokter_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal_dokter', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_dokter')
+            ->constrained('manajemendokter')
+            ->cascadeOnDelete();
+
+            $table->date('tanggal');
+            $table->time('jam_mulai');
+            $table->time('jam_selesai');
+            $table->enum('status', ['tersedia', 'tidak tersedia'])->default('tersedia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal_dokter');
+    }
+};

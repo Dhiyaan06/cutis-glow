96718a3e8d2a38775d981dad77c3548bcfe767c7
Dini Sri Ayu Priyono <dinisriayupriyono@gmail.com>
2026-07-13 07:00:48 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,45 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MasterLayananController;
 use App\Http\Controllers\BookingKonsultasiController;
 use App\Http\Controllers\ManajemenDokterController;
-
+use App\Http\Controllers\ManajemenPasienController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::resource('layanan', MasterLayananController::class);
-Route::resource('booking-konsultasi', BookingKonsultasiController::class);
-Route::resource('dokter', ManajemenDokterController::class);
 
+    // Booking
+    Route::resource('booking', BookingKonsultasiController::class);
+
+    Route::get('/booking-riwayat', [BookingKonsultasiController::class, 'riwayat'])
+        ->name('booking.riwayat');
+
+    Route::put('/booking/{id}/selesai', [BookingKonsultasiController::class, 'selesai'])
+        ->name('booking.selesai');
+});
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::resource('layanan', MasterLayananController::class);
+    Route::resource('pasien', ManajemenPasienController::class);
+    Route::resource('dokter', ManajemenDokterController::class);
+
+});
 
 require __DIR__.'/auth.php';
